010f791882940b27592b0431b7e1a2be2f6e13c2
Gabriel Henrique <GabrielEchelon@users.noreply.github.com>
2019-11-06 10:01:33 -0300

    Update RepeatedString.java

--- src/echelonProject/interviewPreparationKit/RepeatedString.java
+++ src/echelonProject/interviewPreparationKit/RepeatedString.java
@@ -3,21 +3,23 @@ package echelonProject.interviewPreparationKit;
 public class RepeatedString {
 	
 	public static long repeatedString(String pattern, long n) {
-		StringBuilder infinite = new StringBuilder();
-		Long size = n;
-		Long result = 0l;
+		long result = 0l;
 		
-		while((size) > infinite.length()) {
-			infinite.append(pattern);
-		}
-		infinite.substring(0, size.intValue());
-		
-		for(char c : infinite.toString().toCharArray()) {
+		for(char c : s.toCharArray()) {
 			if(c == 'a' || c == 'A') result++;
 		}
 		
-		return result;
+		long size = n / s.length();
+		result =  size * result;
 		
+		long remains = n % s.length();
+		for(int i = 0; i < remains; i++) {
+			if(s.charAt(i) == 'a' || s.charAt(i) == 'A') {
+				result++;
+			}
+		}
+	    
+		return result;
 	}
 
 }
