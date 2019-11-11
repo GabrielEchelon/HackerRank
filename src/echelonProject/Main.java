362ab31789a28a7629b04b372de12f90b7e33d95
Gabriel Henrique <gabriel.henrique@echeloncode.com>
2019-11-10 21:20:06 -0300

    TwoDArrayDS finished

--- src/echelonProject/Main.java
+++ src/echelonProject/Main.java
@@ -22,16 +22,16 @@ public class Main {
 		System.out.println("2D Array DS - Author: Shafet - Difficulty: Easy ");
 		System.out.println("TestCase 0:");
 		
-		int[][] i = {{1,1,1,0,0,0}, 
-					 {0,1,0,0,0,0},
-					 {1,1,1,0,0,0},
-					 {0,0,0,0,0,0},
-					 {0,0,0,0,0,0},
-					 {0,0,0,0,0,0}};
+		int[][] i = {{-9,-9,-9,1,1,1}, 
+					 {0,-9,0,4,3,2},
+					 {-9,-9,-9,1,2,3},
+					 {0,0,8,6,6,0},
+					 {0,0,0,-2,0,0},
+					 {0,0,1,2,4,0}};
 		
 		System.out.println(TwoDArrayDS.solution(i));
 		
-		System.out.println("TestCase 0:");
+		System.out.println("TestCase 1:");
 		
 		int[][] j = {{1,1,1,0,0,0}, 
 					 {0,1,0,0,0,0},
@@ -41,6 +41,28 @@ public class Main {
 					 {0,0,1,2,4,0}};
 	
 		System.out.println(TwoDArrayDS.solution(j));
+		
+		System.out.println("TestCase 2:");
+		
+		int[][] k = {{1,1,1,0,0,0}, 
+					{0,1,0,0,0,0},
+					{1,1,1,0,0,0},
+					{0,9,2,-4,-4,0},
+					{0,0,0,-2,0,0},
+					{0,0,-1,-2,-4,0}};
+		
+		System.out.println(TwoDArrayDS.solution(k));
+		
+		System.out.println("TestCase 3:");
+		
+		int[][] l = {{0,-4,-6,0,-7,-6}, 
+					{-1,-2,-6,-8,-3,-1},
+					{-8,-4,-2,-8,-8,-6},
+					{-3,-1,-2,-5,-7,-4},
+					{-3,-5,-3,-6,-6,-6},
+					{-3,-6,0,-8,-6,-7}};
+		
+		System.out.println(TwoDArrayDS.solution(l));
 		
 	}
 	
--- src/echelonProject/interviewPreparationKit/TwoDArrayDS.java
+++ src/echelonProject/interviewPreparationKit/TwoDArrayDS.java
@@ -3,11 +3,20 @@ package echelonProject.interviewPreparationKit;
 public class TwoDArrayDS {
 	
 	public static int solution(int[][] arr) {
-		int i = 0;
 		
-		//TODO
+		int max = Integer.MIN_VALUE;
+		int sum = 0;
 		
-		return i;
+		for(int line = 0; line <= 3; line++){
+            for(int col = 0; col <= 3; col++){
+                sum = arr[line + 1][col + 1];
+                for(int aux = col; aux < col + 3; aux++){
+                    sum += arr[line][aux] + arr[line + 2][aux];
+                }            
+                if(sum > max)
+                    max = sum;
+            }
+        }
+		return max;
 	}
-
 }
